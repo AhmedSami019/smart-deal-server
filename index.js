40e21f4123e648c6b8489af24a6a8d0883ff2389
ahmedsami019 <easami019192@gmail.com>
2026-04-07 12:35:15 +0600

    init the app

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res)=>{
+    res.send('hello icy')
+})
+
+app.listen(port, ()=>{
+    console.log(`the server is running from port ${port}`);
+})
