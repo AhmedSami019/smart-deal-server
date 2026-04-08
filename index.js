8534dc22532433503784a9ea1148497d42ea42d3
ahmedsami019 <easami019192@gmail.com>
2026-04-08 14:01:45 +0600

    added api to get single bid

--- index.js
+++ index.js
@@ -81,6 +81,13 @@ async function run() {
             res.send(result)
         })
 
+        app.get('/bids/:id', async(req, res)=>{
+            const id = req.params.id
+            const query = {_id : new ObjectId(id)}
+            const result = await bidsCollection.findOne(query)
+            res.send(result)
+        })
+
 
 
         await client.db('admin').command({ping : 1});
